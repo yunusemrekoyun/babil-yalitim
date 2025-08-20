e7a36750240f8f9179ce1b82ad95bd26bfafae4e
Yunus Emre Koyun <yunusemrekoyun26@gmail.com>
2025-08-20 18:35:05 +0300

    .

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -9,20 +9,20 @@ import ServiceSection from "../components/Service/ServiceSection";
 import AboutSection from "../components/About/AboutSection";
 import GlassSection from "../components/Layout/GlassSection";
 import BlogGrid from "../components/Blog/BlogGrid";
-import BackgroundVideo from "../components/Background/BackgroundVideo";
+// import BackgroundVideo from "../components/Background/BackgroundVideo";
 
-const HERO_DESKTOP = import.meta.env.VITE_HERO_PUBLIC_ID;
-const HERO_MOBILE = import.meta.env.VITE_HERO_MOBILE_PUBLIC_ID;
-const HERO_POSTER = import.meta.env.VITE_HERO_POSTER_PUBLIC_ID;
+// const HERO_DESKTOP = import.meta.env.VITE_HERO_PUBLIC_ID;
+// const HERO_MOBILE = import.meta.env.VITE_HERO_MOBILE_PUBLIC_ID;
+// const HERO_POSTER = import.meta.env.VITE_HERO_POSTER_PUBLIC_ID;
 
 export default function HomePage() {
   return (
     <div className="relative min-h-screen overflow-x-hidden">
-      <BackgroundVideo
+      {/* <BackgroundVideo
         desktopPublicId={HERO_DESKTOP}
         mobilePublicId={HERO_MOBILE}
         posterPublicId={HERO_POSTER}
-      />
+      /> */}
 
       <div className="relative z-10">
         {/* Navbar’a bir id verelim ki yüksekliğini hesaplayabilelim (opsiyonel) */}
